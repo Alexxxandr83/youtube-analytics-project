f949c859e841a04343e80be274321ff0b0e95cdb
Alexandp L <alexandp83@mail.ru>
2023-04-23 20:46:33 +0300

    что-то скопипастил из youtube_api_manual, но что это и как работает, нифига не ясно!

--- homework-1/src/channel.py
+++ homework-1/src/channel.py
@@ -1,6 +1,20 @@
+import json
+import os
+
+from googleapiclient.discovery import build
+
+
+# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+api_key: str = os.getenv('API_KEY')
+
+# создать специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
 class Channel:
     def __init__(self, channel_id):
         self.channel_id = channel_id
 
-    def print_info(self):
-        pass
+    def print_info(dict_to_print: dict) -> None:
+        """Выводит словарь в json-подобном удобном формате с отступами"""
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
