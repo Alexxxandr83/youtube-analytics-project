a462177ad1458bc13d5fca213aba133a6173042a
Alexandp L <alexandp83@mail.ru>
2023-04-25 22:05:34 +0300

    Сделана домашка 2

--- homework-2/main.py
+++ homework-2/main.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     # можем получить объект для работы с API вне класса
     print(Channel.get_service())
-    # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
-
-    # создаем файл 'vdud.json' в данными по каналу
+    # # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
+    #
+    # # создаем файл 'vdud.json' в данными по каналу
     vdud.to_json('vdud.json')
